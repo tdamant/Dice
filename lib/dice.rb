b073b0f7f3857d998eedffb99c5e03ab73eb37f9
Tom Damant <tom@gluon.local>
2019-01-10 11:19:49 +0000

    fourth user story feature test passed, user can determine number of die to be rolled

--- lib/dice.rb
+++ lib/dice.rb
@@ -1,5 +1,7 @@
 class Dice
-  def roll
-    rand(1..6)
+  def roll(n=1)
+    array = []
+    n.times{array << rand(1..6)}
+    array
   end
 end
--- spec/dice_spec.rb
+++ spec/dice_spec.rb
@@ -2,20 +2,19 @@ require 'dice'
 
 describe Dice do
   it "rolling die should an integer" do
-    expect(subject.roll).to be_an_instance_of Integer
+    expect(subject.roll[0]).to be_an_instance_of Integer
   end
 
   it "should always return number between 1 and 6" do
-    arr_1 = []
-    100.times{arr_1 << subject.roll}
-    expect(arr_1.select{|n| n < 1 || n > 6}).to eq []
+    expect(subject.roll(100).select{|n| n < 1 || n > 6}).to eq []
   end
 
-  it "the die should be random" do
-    arr_1 =[] ; arr_2 = []
-    100.times{arr_1 << subject.roll}
-    100.times{arr_2 << subject.roll}
-    expect(arr_1).not_to eq arr_2
+  it "should be random" do
+    srand(4)
+    expect(subject.roll).to eq [3]
   end
 
+  it "user should be able to specify the number of die to roll" do
+    expect(subject.roll(4).length).to eq 4
+  end
 end
