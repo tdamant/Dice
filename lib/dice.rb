59e3473fc6b58257a25cb46e72df944a97eeb37b
Tom Damant <tom@gluon.local>
2019-01-10 10:32:52 +0000

    third user story feature test working, dice rolls are random

--- lib/dice.rb
+++ lib/dice.rb
@@ -1,5 +1,5 @@
 class Dice
   def roll
-    2
+    rand(1..6)
   end
 end
--- spec/dice_spec.rb
+++ spec/dice_spec.rb
@@ -1,9 +1,21 @@
 require 'dice'
+
 describe Dice do
-  it "should respond to the roll method" do
-    expect(subject).to respond_to :roll
+  it "rolling die should an integer" do
+    expect(subject.roll).to be_an_instance_of Integer
   end
-  it "rolling die should return number in 1 - 6" do
-    expect(subject.roll).to be_between(1,6)
+
+  it "should always return number between 1 and 6" do
+    arr_1 = []
+    100.times{arr_1 << subject.roll}
+    expect(arr_1.select{|n| n < 1 || n > 6}).to eq []
   end
+
+  it "the die should be random" do
+    arr_1 =[] ; arr_2 = []
+    100.times{arr_1 << subject.roll}
+    100.times{arr_2 << subject.roll}
+    expect(arr_1).not_to eq arr_2
+  end
+
 end
